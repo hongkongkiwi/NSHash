875f1ad928b6997a74effeff1891ee191000cefa
Andy <andy@savage.hk>
2015-04-28 10:03:46 +0800

    Updated NSHash to include an iOS Framework

---
+++ NSHash iOS/NSHash iOS.h
@@ -0,0 +1,19 @@
+//
+//  NSHash iOS.h
+//  NSHash iOS
+//
+//  Created by Andy on 28/4/15.
+//  Copyright (c) 2015 buenocode. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for NSHash iOS.
+FOUNDATION_EXPORT double NSHash_iOSVersionNumber;
+
+//! Project version string for NSHash iOS.
+FOUNDATION_EXPORT const unsigned char NSHash_iOSVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <NSHash_iOS/PublicHeader.h>
+
+
